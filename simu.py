d6f244fbc0e7898d523a573b9f2b03da5b174e47
Adam Kocoloski <adam.kocoloski@gmail.com>
2008-09-19 14:01:32 -0400

    add mcasym(), sample IDs for 2005, and few other bugfixes

--- simu.py
+++ simu.py
@@ -1,6 +1,6 @@
 import ROOT
 import math
-# // $Id$
+import os.path
 
 xsec = { 
 '2_3'       : 8.150,
@@ -18,7 +18,23 @@ xsec = {
 '55_65'     : 1.451E-09
 }
 
-run6_samples = {
+samples = {
+### 2005
+'rcf1224'   : '5_7',
+'rcf1225'   : '7_9',
+'rcf1226'   : '9_11',
+'rcf1227'   : '11_15',
+'rcf1228'   : '15_25',
+'rcf1229'   : '25_35',
+'rcf1230'   : 'above_35',
+'rcf1231'   : '2_3',
+'rcf1232'   : '3_4',
+'rcf1233'   : '4_5',
+'rcf1235'   : 'minbias', ## ?
+'rcf1270'   : '45_55',
+'rcf1271'   : '55_65',
+'rcf1273'   : '0_2',
+### 2006
 'rcf1302'   : '45_55',
 'rcf1303'   : '35_45',
 'rcf1304'   : '55_65',
@@ -44,35 +60,35 @@ def mergeHistos(histos, eventCounts, sampleIds):
         for bin in range(1, nbins+1):
             content = out.GetBinContent(bin)
             error = out.GetBinError(bin)**2
-            if h is None: 
-                print 'wtf! why is this histo None?'
-                continue
             nparticles = h.GetBinContent(bin)
-            # if nparticles >= minCounts:
             content += nparticles * wp
             error   += wp * wp * nparticles * (1+nparticles/nevents)
             out.SetBinContent(bin,content)
             out.SetBinError(bin, math.sqrt(error))
     return out
 
-
 def mergeSamples(outName, inputFileNames):
     outFile = ROOT.TFile(outName, 'recreate')
     inputFiles = [ROOT.TFile(n) for n in inputFileNames]
     eventCounts = [f.Get('eventCounter').GetEntries() for f in inputFiles]
-    ## this will throw an exception on run5 samples
-    sampleIds = [run6_samples[n[:7]] for n in inputFileNames]
+    sampleIds = [samples[os.path.basename(n)[:7]] for n in inputFileNames]
     
     keys = inputFiles[0].GetListOfKeys()
     for key in keys:
-        print key
         if key.GetName() == 'eventCounter': continue
         histos = [f.Get(key.GetName()) for f in inputFiles]
         
         # skip 2D histos for now b/c they are SLOW
         if histos[0].ClassName().startswith('TH2'): 
-            [h.Delete() for h in histos]
-            continue
+            ## but keep 'z', 'z_away'
+            keepMe = False
+            for name in ('_z', '_z_away'):
+                if name in histos[0].GetName():
+                    keepMe = True
+            if not keepMe:
+                [h.Delete() for h in histos]
+                continue
+        print key
         
         out = mergeHistos(histos, eventCounts, sampleIds)
         [h.Delete() for h in histos]
@@ -81,8 +97,91 @@ def mergeSamples(outName, inputFileNames):
     
     outFile.Close()
     
+def mcasym(outName, inputFileNames, trigger='jetpatch', keys=None):
+    """
+    caveats:
+    select different nparticles histos for 05(pt) and 06(z_away2.Rebin())
+    """
+    outFile = ROOT.TFile(outName, 'recreate')
+    inputFiles = [ROOT.TFile(n) for n in inputFileNames]
+    keys = keys or ['STD','MAX','MIN','ZERO','GS_NLOC']
+    for key in keys:
+        minus_inputs = map(lambda f: \
+        {
+            'id': samples[os.path.basename(f.GetName())[:7]],
+            'xsec': xsec[samples[os.path.basename(f.GetName())[:7]]],
+            'nevents': f.Get('eventCounter').GetEntries(),
+            'num': f.Get('_%s_anyspin_minus_%s' % (trigger, key)),
+            'denom': f.Get('_%s_anyspin_minus_denom' % trigger),
+            'nparticles': f.Get('_%s_anyspin_minus_pt' % trigger)
+            # 'nparticles': f.Get('_%s_anyspin_minus_z_away2' % trigger).Rebin()
+        }, inputFiles)
+        plus_inputs = map(lambda f: \
+        {
+            'id': samples[os.path.basename(f.GetName())[:7]],
+            'xsec': xsec[samples[os.path.basename(f.GetName())[:7]]],
+            'nevents': f.Get('eventCounter').GetEntries(),
+            'num': f.Get('_%s_anyspin_plus_%s' % (trigger, key)),
+            'denom': f.Get('_%s_anyspin_plus_denom' % trigger),
+            'nparticles': f.Get('_%s_anyspin_plus_pt' % trigger)
+            # 'nparticles': f.Get('_%s_anyspin_plus_z_away2' % trigger).Rebin()
+        }, inputFiles)
+        for m in minus_inputs:
+            print m['id'], m['xsec'], m['nevents'], m['nparticles'].GetEntries()
+        outFile.cd()
+        _mcasym_merge(minus_inputs).Write()
+        _mcasym_merge(plus_inputs).Write()
+    outFile.Close()
+        
+def _mcasym_merge(inputs):
+    out = inputs[0]['num'].Clone()
+    out.Reset('ice')
+    
+    ## don't include a bin from an individual sample in content or error
+    ## if it has fewer than this # of particles
+    minParticlesToAccept = 10
+    
+    ## first do the bin contents
+    bottom = []
+    for bin in range(1, out.GetNbinsX()):
+        top = 0.0
+        bot = 0.0
+        error = 0.0
+        for sample in inputs:
+            nparticles = sample['nparticles'].GetBinContent(bin)
+            if nparticles < minParticlesToAccept: continue
+            wp = sample['xsec']/sample['nevents']
+            top += sample['num'].GetBinContent(bin) * wp
+            bot += sample['denom'].GetBinContent(bin) * wp
+        
+        bottom.append(bot)
+        content = bot > 0 and (top/bot) or 0.0
+        out.SetBinContent(bin, content)
+    
+    ## now do the errors
+    for bin in range(1, out.GetNbinsX()):
+        error = 0.0
+        
+        for sample in inputs:
+            nparticles = sample['nparticles'].GetBinContent(bin)
+            if nparticles < minParticlesToAccept: continue
+            wp = sample['xsec']/sample['nevents']
+            nevents = sample['nevents']
+            term0 = sample['num'].GetBinContent(bin)/nparticles
+            term1 = term0 - out.GetBinContent(bin)
+            term2 = sample['num'].GetBinError(bin)**2/nparticles - term0**2
+            error += wp*wp*nparticles*(term1*term1*(1+nparticles/nevents)+term2)
+        
+        ferror = bottom[bin-1]>0 and math.sqrt(error/(bottom[bin-1]**2)) or 0.0
+        out.SetBinError(bin, ferror)
+        print bin, out.GetBinError(bin)
+        
+    return out
+    
+    
 def partonicCrossSection(sample='2_3', nevents=1000, sqrts=200):
-    """runs standalone Pythia to determine xsec for weighting purposes, returns xsec"""
+    """runs standalone Pythia to determine xsec for weighting purposes.
+    returns xsec"""
     import ROOT
     pythia = ROOT.TPythia6()
     
@@ -128,7 +227,3 @@ def partonicCrossSection(sample='2_3', nevents=1000, sqrts=200):
     
     return pythia.GetPARI(1)
 
-
-# /*****************************************************************************
-#  * $Log$
-#  *****************************************************************************/
