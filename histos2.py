49c66a8b0ec16cd2e777f60da81bffc4198e7831
Adam Kocoloski <kocolosk@mit.edu>
2009-02-04 21:58:10 -0500

    include 2_3 sample in condor_simu runs

--- histos2.py
+++ histos2.py
@@ -421,7 +421,7 @@ def condor_simu(treeDir, trigList=None, modlist=None, histdir='./'):
     uniq = sets.Set(prefixes)
     
     ## ignore these samples until further notice
-    ignored = ('rcf1231', 'rcf1235', 'rcf1270', 'rcf1271', 'rcf1273')
+    ignored = ('rcf1235', 'rcf1270', 'rcf1271', 'rcf1273')
     for elem in ignored:
         try:
             uniq.remove(elem)
