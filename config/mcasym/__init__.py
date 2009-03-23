80f33437068d6f136e1c403226d0b2547cadde97
Adam Kocoloski <kocolosk@mit.edu>
2009-03-23 12:56:34 -0400

    add asymmetries plotted using mcTracks()

--- config/mcasym/__init__.py
+++ config/mcasym/__init__.py
@@ -1,3 +1,4 @@
-modules = ['pt', 'z', 'reweighted', 'reweighted_high', 'reweighted_low']
+modules = ['pt', 'z', 'reweighted', 'reweighted_high', 'reweighted_low', 
+    'pt_true']
 __import__(__name__, {}, {}, modules)
 
